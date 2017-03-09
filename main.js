8d0298ebff73408ebae42d51d90015610c66ac6c
NEAL STUDZINSKI <nealstudzinski@gmail.com>
2017-03-09 07:49:19 -0600

    added some style to css

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-var calculate = document.querySelector ('#button');
-console.dir (calculate);
-
 var firstNumber = document.querySelector ('#input1');
 console.dir (firstNumber);
 
 var secondNumber = document.querySelector ('#input2');
 console.dir (secondNumber);
+
+var calculate = document.querySelector ('#button');
+console.dir (calculate);
 
 var sum = document.querySelector ('#output');
 console.dir (sum)
